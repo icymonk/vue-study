a2146be2e77e8294473de278fe657d411dc29831
kevin-ddh <cjkim@ddhinc.net>
2019-04-17 21:26:00 +0900

    Update: 4/17 노원 1차

---
+++ lecture03/노원/calculator/src/components/VModal.vue
@@ -0,0 +1,46 @@
+<style lang="less" scoped>
+button {
+  background: none;
+  padding: 16px 32px;
+  border-radius: 8px;
+  &:focus {
+    outline: none;
+  }
+}
+button:focus {
+  outline: none;
+}
+.modal-cover {
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  position: absolute;
+  top: 0;
+  height: 100%;
+  z-index: 2;
+}
+.modal {
+  width: 400px;
+  height: 400px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: 1px solid grey;
+  background: white;
+  border-radius: 16px;
+  padding: 32px;
+  z-index: 3;
+  box-shadow: 4px 4px 4px 4px #666;
+}
+.modal-header {
+  font-size: 24px;
+  border-bottom: 1px solid grey;
+  margin-bottom: 8px;
+}
+.modal-footer {
+  position: absolute;
+  bottom: 0;
+  padding: 24px;
+  width: 100%;
+}
+</style>
