e745e47dccc3e289a6ee8d35fbebda3988edc375
kevin-ddh <cjkim@ddhinc.net>
2019-04-23 22:37:44 +0900

    Update: 4/17 노원 calculator

--- lecture03/노원/calculator/src/App.vue
+++ lecture03/노원/calculator/src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="calculator">
+      <div id="screen">
+        <input type="text" v-model="screenValue">
+      </div>
+      <div id="body">
+        <button v-for="key in keyList" :key="key" @click="inputKey(key)">{{ key }}</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "app",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      screenValue: "",
+      keyList: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    };
+  },
+  methods: {
+    inputKey(key) {
+      this.screenValue += key;
+    }
   }
 };
 </script>
 
-<style lang="less">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped lang="less">
+#calculator {
+  display: flex;
+  flex-direction: column;
+  #screen > input {
+    width: 100%;
+    height: 60px;
+    text-align: right;
+    font-size: 40px;
+  }
+  #body {
+    display: flex;
+    flex-direction: column;
+    padding-top: 8px;
+    .row {
+      display: flex;
+
+      justify-content: space-around;
+    }
+  }
+}
+button {
+  float: left;
+  width: 25%;
+  font-size: 24px;
+  height: 120px;
+  background: none;
+  border: 1px solid grey;
+  &:focus {
+    outline: none;
+  }
 }
 </style>
--- lecture03/노원/calculator/src/components/VModal.vue
+++ lecture03/노원/calculator/src/components/VModal.vue
@@ -1,3 +1,29 @@
+<template>
+  <div class="modal-cover" @click="$emit('toggle-modal')">
+    <div class="modal" @click.stop>
+      <div class="modal-header">
+        {{ title }}
+        <slot name="header"></slot>
+      </div>
+      <slot></slot>
+      <div class="modal-footer">
+        <slot name="footer"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "Modal title"
+    }
+  }
+};
+</script>
+
 <style lang="less" scoped>
 button {
   background: none;
@@ -7,9 +33,6 @@ button {
     outline: none;
   }
 }
-button:focus {
-  outline: none;
-}
 .modal-cover {
   background: rgba(0, 0, 0, 0.5);
   width: 100%;
@@ -17,30 +40,30 @@ button:focus {
   top: 0;
   height: 100%;
   z-index: 2;
-}
-.modal {
-  width: 400px;
-  height: 400px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  border: 1px solid grey;
-  background: white;
-  border-radius: 16px;
-  padding: 32px;
-  z-index: 3;
-  box-shadow: 4px 4px 4px 4px #666;
-}
-.modal-header {
-  font-size: 24px;
-  border-bottom: 1px solid grey;
-  margin-bottom: 8px;
-}
-.modal-footer {
-  position: absolute;
-  bottom: 0;
-  padding: 24px;
-  width: 100%;
+  .modal {
+    width: 400px;
+    height: 400px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border: 1px solid grey;
+    background: white;
+    border-radius: 16px;
+    padding: 32px;
+    z-index: 3;
+    box-shadow: 4px 4px 4px 4px #666;
+    .modal-header {
+      font-size: 24px;
+      border-bottom: 1px solid grey;
+      margin-bottom: 8px;
+    }
+    .modal-footer {
+      position: absolute;
+      bottom: 0;
+      padding: 24px;
+      width: 100%;
+    }
+  }
 }
 </style>
